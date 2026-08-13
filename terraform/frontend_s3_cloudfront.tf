5a0d4d177a61043366385ed648a31da2e5a6fa4d
yshmael <yshmael.malik@gmail.com>
2026-08-13 18:35:13 +0800

    security: scope CloudFront invalidation permissions to specific ARN

--- terraform/frontend_s3_cloudfront.tf
+++ terraform/frontend_s3_cloudfront.tf
@@ -119,8 +119,8 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
-        Effect    = "Allow"
+        Sid    = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect = "Allow"
         Principal = {
           Service = "cloudfront.amazonaws.com"
         }
